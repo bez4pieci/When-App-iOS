508f098b05e9147905510553a068787c6066b731
bez4pieci <ernests@golayer.io>
2025-05-24 14:30:50 +0200

    Initial Commit

---
+++ DeparturesTests/DeparturesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeparturesTests.swift
+//  DeparturesTests
+//
+//  Created by Ernests Karlsons on 24.05.25.
+//
+
+import Testing
+@testable import Departures
+
+struct DeparturesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
